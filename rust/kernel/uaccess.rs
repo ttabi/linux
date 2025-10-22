c869dce93b87da722ab6c1bda25e99d2219a5241
Danilo Krummrich <dakr@kernel.org>
2025-10-22 16:30:36 +0200

    rust: uaccess: add UserSliceReader::read_slice_partial()
    The existing read_slice() method is a wrapper around copy_from_user()
    and expects the user buffer to be larger than the destination buffer.
    
    However, userspace may split up writes in multiple partial operations
    providing an offset into the destination buffer and a smaller user
    buffer.
    
    In order to support this common case, provide a helper for partial
    reads.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Matthew Maurer <mmaurer@google.com>
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Message-ID: <20251022143158.64475-3-dakr@kernel.org>

--- rust/kernel/uaccess.rs
+++ rust/kernel/uaccess.rs
@@ -287,6 +287,22 @@ impl UserSliceReader {
         self.read_raw(out)
     }
 
+    /// Reads raw data from the user slice into a kernel buffer partially.
+    ///
+    /// This is the same as [`Self::read_slice`] but considers the given `offset` into `out` and
+    /// truncates the read to the boundaries of `self` and `out`.
+    ///
+    /// On success, returns the number of bytes read.
+    pub fn read_slice_partial(&mut self, out: &mut [u8], offset: usize) -> Result<usize> {
+        let end = offset
+            .checked_add(self.len())
+            .unwrap_or(out.len())
+            .min(out.len());
+
+        out.get_mut(offset..end)
+            .map_or(Ok(0), |dst| self.read_slice(dst).map(|()| dst.len()))
+    }
+
     /// Reads a value of the specified type.
     ///
     /// Fails with [`EFAULT`] if the read happens on a bad address, or if the read goes out of
