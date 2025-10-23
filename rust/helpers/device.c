8f16c7ffe9d9edee794f7c189a1bbb04fd2c7a02
Timur Tabi <ttabi@nvidia.com>
2025-10-23 14:33:27 -0500

    rust: pci: add device name method
    Add a name() method to the `Device` type, which returns a CStr that
    contains the device name, typically the BDF address for PCI devices.
    
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- rust/helpers/device.c
+++ rust/helpers/device.c
@@ -25,3 +25,8 @@ void rust_helper_dev_set_drvdata(struct device *dev, void *data)
 {
 	dev_set_drvdata(dev, data);
 }
+
+const char *rust_helper_dev_name(const struct device *dev)
+{
+	return dev_name(dev);
+}
