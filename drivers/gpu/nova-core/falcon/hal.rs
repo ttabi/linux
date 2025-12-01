8550bcba0d2fc8c9e38de6268563d700e641fa80
Timur Tabi <ttabi@nvidia.com>
2025-12-01 16:19:17 -0600

    gpu: nova-core: add Falcon HAL method load_method()
    Some GPUs do not support using DMA to transfer code/data from system
    memory to Falcon memory, and instead must use programmed I/O (PIO).
    Add a function to the Falcon HAL to indicate whether a given GPU's
    Falcons support DMA for this purpose.
    
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- drivers/gpu/nova-core/falcon/hal.rs
+++ drivers/gpu/nova-core/falcon/hal.rs
@@ -15,6 +15,16 @@ use crate::{
 mod ga102;
 mod tu102;
 
+/// Method used to load data into falcon memory. Some GPU architectures need
+/// PIO and others can use DMA.
+#[expect(unused)]
+pub(crate) enum LoadMethod {
+    /// Programmed I/O
+    Pio,
+    /// Direct Memory Access
+    Dma,
+}
+
 /// Hardware Abstraction Layer for Falcon cores.
 ///
 /// Implements chipset-specific low-level operations. The trait is generic against [`FalconEngine`]
@@ -48,6 +58,10 @@ pub(crate) trait FalconHal<E: FalconEngine>: Send + Sync {
 
     /// Reset the falcon engine.
     fn reset_eng(&self, bar: &Bar0) -> Result;
+
+    /// returns the method needed to load data into Falcon memory
+    #[expect(unused)]
+    fn load_method(&self) -> LoadMethod;
 }
 
 /// Returns a boxed falcon HAL adequate for `chipset`.
