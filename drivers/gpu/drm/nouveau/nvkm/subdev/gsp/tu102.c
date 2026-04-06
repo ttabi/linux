5955687b75fe8fa95bfbe573243e2de65d217b08
Timur Tabi <ttabi@nvidia.com>
2026-04-06 14:32:14 -0500

    drm/nouveau: check for GA100 specifically when calculating FRTS size
    When booting with GSP-RM, the FRTS data region normally needs to be
    allocated.  However, on GA100, this region is not used and so its
    size needs to be set to zero.  The current method of looking at
    device->bios is not valid, as GA100 actually does have a VBIOS.
    
    The truth is that GA100 is just special, and the simplest way to
    determine the proper FRTS data region size is to check for this
    GPU specifically.
    
    Fixes: 20e0c197802c ("drm/nouveau/gsp: add support for GA100")
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/gsp/tu102.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/gsp/tu102.c
@@ -319,12 +319,12 @@ tu102_gsp_oneinit(struct nvkm_gsp *gsp)
 		return ret;
 
 	/*
-	 * Calculate FB layout. FRTS is a memory region created by the FWSEC-FRTS firmware.
-	 * FWSEC comes from VBIOS.  So on systems with no VBIOS (e.g. GA100), the FRTS does
-	 * not exist.  Therefore, use the existence of VBIOS to determine whether to reserve
-	 * an FRTS region.
+	 * Calculate FB layout. FRTS is a memory region created by running the FWSEC-FRTS
+	 * command, which writes power management data into WPR2. On GA100, the booter
+	 * firmware handles WPR2 setup directly and FRTS data is not needed, so no FRTS
+	 * region is reserved.
 	 */
-	gsp->fb.wpr2.frts.size = device->bios ? 0x100000 : 0;
+	gsp->fb.wpr2.frts.size = device->chipset == 0x170 ? 0 : 0x100000;
 	gsp->fb.wpr2.frts.addr = ALIGN_DOWN(gsp->fb.bios.addr, 0x20000) - gsp->fb.wpr2.frts.size;
 
 	gsp->fb.wpr2.boot.size = gsp->boot.fw.size;
